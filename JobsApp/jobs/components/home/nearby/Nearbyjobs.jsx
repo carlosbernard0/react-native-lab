d3b674a06f4c207fae01b58f213002fd3b0e0efa
carlosbernard0 <carlosbernardo774@gmail.com>
2024-09-13 15:39:57 -0300

    start job details screen

--- JobsApp/jobs/components/home/nearby/Nearbyjobs.jsx
+++ JobsApp/jobs/components/home/nearby/Nearbyjobs.jsx
@@ -36,7 +36,7 @@ const Nearbyjobs = () => {
             <NearbyJobCard
               job={job}
               key={`nearby-job-${job?.job_id}`}
-              handleNavigate={()=> router.push(`/job-datails/${job.job_id}`)}
+              handleNavigate={()=> router.push(`/job-details/${job.job_id}`)}
             />
           ))
         )}
--- JobsApp/jobs/src/app/job-details/[id].js
+++ JobsApp/jobs/src/app/job-details/[id].js
@@ -1,8 +1,67 @@
-import {Text} from "react-native";
+import {Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl, RefreshControlComponent} from "react-native";
+import { Stack, useRouter, useLocalSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from "../../../components";
+import { COLORS, icons, SIZES } from "../../../constants";
+import useFetch from "../../../hook/useFetch";
 
 const JobDetails = () => {
+    const params = useLocalSearchParams()
+    const router = useRouter()
+
+    const { data, isLoading, error, refetch} = useFetch("job-details", {
+        job_id: params.id
+    })
+    
+    const [refreshing, setRefreshing] = useState(false)
+
+    const onRefresh = () => {}
+
     return(
-        <Text>JobDetails</Text>
+        <SafeAreaView style={ { flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen
+                options={{headerStyle : {backgroundColor: COLORS.lightWhite},
+                headerShadowVisible: false,
+                headerBackVisible: false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn
+                        iconUrl={icons.left}
+                        dimension="60%"
+                        handlePress={() => router.back()}
+                    />
+                ),
+                headerRight: () => (
+                    <ScreenHeaderBtn
+                        iconUrl={icons.share}
+                        dimension="60%"
+                    />
+                ),
+                headerTitle: ""
+            }}  
+            >
+            
+            <>
+                <ScrollView showsHorizontalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing}  onRefresh={onRefresh} />}>
+
+                    {isLoading ? (
+                        <ActivityIndicator size="large" color={COLORS.primary}/>
+                    ): error ? (
+                        <Text>Something went wrong</Text>                        
+                    ) : data.length===0 ? (
+                        <Text>No data</Text>
+                    ) : (
+                        <View style={{padding: SIZES.medium, paddingBottom: 100}}>
+                            <Company/>
+                        </View>
+                    )}
+
+                </ScrollView>
+            </>
+
+            </Stack.Screen>
+        </SafeAreaView>
+
     )
 }
 
